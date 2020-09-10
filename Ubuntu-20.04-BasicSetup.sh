454d4b1b60fa4e083fe7d7f6143de441b7c821ae
Filippo Cenobi <11414315+fcenobi@users.noreply.github.com>
2020-09-10 04:07:50 +0200

    Update Ubuntu-20.04-BasicSetup.sh

--- Ubuntu-20.04-BasicSetup.sh
+++ Ubuntu-20.04-BasicSetup.sh
@@ -185,6 +185,29 @@ sudo apt -y install traceroute
 sudo apt -y install tree 
 sudo apt -y install unar 
 
+sudo apt install xrdp
+sudo apt install -y xfce4
+#scegliere gdm3
+sudo apt install -y xfce4-goodies
+
+
+sudo cp /etc/xrdp/xrdp.ini /etc/xrdp/xrdp.ini.bak
+sudo sed -i 's/3389/3390/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/max_bpp=32/#max_bpp=32\nmax_bpp=128/g' /etc/xrdp/xrdp.ini
+sudo sed -i 's/xserverbpp=24/#xserverbpp=24\nxserverbpp=128/g' /etc/xrdp/xrdp.ini
+echo xfce-session > ~/.xsession
+
+
+sudo sed  -i 's/test -x \/etc\/X11\/Xsession && exec \/etc\/X11\/Xsession/#test -x \/etc\/X11\/Xsession && exec \/etc\/X11\/Xsession/g' /etc/xrdp/startwm.sh
+sudo sed  -i 's/exec \/bin\/sh \/etc\/X11\/Xsession/#exec \/bin\/sh \/etc\/X11\/Xsession/g'  /etc/xrdp/startwm.sh
+
+echo "# xfce" >> /etc/xrdp/startwm.sh
+echo startxfce4 >> /etc/xrdp/startwm.sh
+
+
+
+
+
 install_python3_pip() {
   sudo apt install -y python3-pip
 }
